74514e398e1fb3137e7782a9b633945ed85127d5
NoHomey <ivo.stratev.tues@gmail.com>
2021-06-08 20:05:34 +0300

    Adding impl for Kruskal's algorithm

---
+++ graph/mst/kruskal/kruskal.cpp
@@ -0,0 +1,73 @@
+#include <vector>
+#include <algorithm>
+#include "./union-find/union-find.h"
+
+#include <iostream>
+
+using Weight = int;
+
+using Vertex = unsigned int;
+
+struct Edge {
+    Vertex from;
+    Vertex to;
+    Weight weight;
+};
+
+using Graph = std::vector<Edge>;
+
+struct Result {
+    bool valid;
+    Graph tree;
+    Weight total;
+};
+
+bool lessWeight(const Edge& a, const Edge& b) {
+    return a.weight < b.weight;
+}
+
+void sortEdgesByWeight(Graph& graph) {
+    // If needed we use Heap Sort !!!
+    std::sort(graph.begin(), graph.end(), lessWeight); // Theta(n * lg n), Theta(1)
+}
+
+Result kruskal(Graph& graph) {
+    std::size_t n = graph.size();
+    Graph tree;
+    tree.reserve(n - 1);
+    Result res = {true, std::move(tree), 0};
+    std::size_t limit = 1;
+    UnionFind forest(n); // Theta(n), Theta(n)
+
+    sortEdgesByWeight(graph); // Theta(m * lg m), Theta(1)
+    for(const Edge& e: graph) { // Theta(m), Theta(1)
+        if(limit == n) {
+            break;
+        }
+        if(forest.Find(e.from) != forest.Find(e.to)) {
+            forest.Union(e.from, e.to);
+            res.tree.push_back(e);
+            res.total += e.weight;
+            limit++;
+        }
+    }
+
+    if(res.tree.size() != n - 1) {
+        res.valid = false;
+    }
+    return res;
+} // Theta(m * lg m), Theta(n)
+
+//  0    
+//  | \
+//  10 \
+//  |   3    
+//  |     \ 
+//  1 --10--2
+
+int main() {
+    Graph g = {{0, 1, 10}, {0, 2, 3}, {1, 2, 10}};
+    Result res = kruskal(g);
+    std::cout << res.valid << ":" << res.total << std::endl;
+    return 0;
+}
